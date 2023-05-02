24c8b5744997f1caaa097df857bb2919510eda60
Nithesh K <124651925+NitheshNSK@users.noreply.github.com>
2023-05-02 22:20:18 +0530

    description for mini projects

--- README.m
+++ README.m
@@ -1,2 +1,4 @@
 # miniProject
 added to mini projects
+1.color picker game created by usind HTML Js and CSS added an eventListener and make changes using DOM manupulation
+2.TODO created by using HTML Js and CSS (on progress not completed fully)
