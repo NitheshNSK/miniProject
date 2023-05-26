29877d3aa6a23fc28b9b3c97943fe619b68a1527
NitheshNSK <sahashnsk@gmail.com>
2023-05-27 00:32:53 +0530

    strike is implemented

--- TODO application/script.js
+++ TODO application/script.js
@@ -1,36 +1,22 @@
-let dateTime=document.getElementById("calender-time");
-let ul=document.getElementById("todo-list");
-let form=document.querySelector("form");
-let clearBtn=document.getElementById("clear")
-let dates =new Date().toLocaleDateString("en-us", { weekday: "long", year: "numeric", month:"short", day: "numeric" });
-let id=1;
-dateTime.textContent=dates;
+init=()=>{
+    let dateTime=document.getElementById("calender-time");
+    let dates =new Date().toLocaleDateString("en-us", { weekday: "long", year: "numeric", month:"short", day: "numeric" });
+    dateTime.textContent=dates;
+}
+let index=1;
+addToDom=(text)=>{
+    let todo_list= document.getElementById("todo-list");
+    todo_list.innerHTML+=`<li class="list-group-item p-3 " >
+                            <input class="form-check-input me-1" id="todo-${index}"  type="checkbox">
+                             <label class="form-check-label" id="id-${index}" for="todo-${index}">
+                                ${text}
+                            </label>
+                        </li>`
+    index+=1;
+                    }
+updateDom=(data)=>{
+    let holder=data.innerText;
+    data.innerHTML=holder.strike();
+    data.setAttribute("style","pointer-events:none");
 
-form.addEventListener("submit",(e)=>{
-    e.preventDefault();
-    let {Input}=form.elements;
-    // let obj={
-    //     input_ele:Input.value,
-    //     submitbtn:SubmintButton.value,
-    //     clearBtn:clear.value
-    // }
-    addToDOM (Input.value);
-})
-clearBtn.addEventListener("click",()=>{
-    ul.innerHTML=``
-})
-
-
-
-let addToDOM=(value)=>{
-    
-
-   ul.innerHTML+=`<li class="list-group-item p-3">
-                    <input class="form-check-input me-1"  type="checkbox">
-                    <label class="form-check-label" id="todo-${id}"  for="todo-${id}">
-                    ${value}
-                </label>
-                </li>`
-    id+=1;
-    return value;
 }
