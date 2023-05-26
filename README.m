1e9466c674ce4974e7a831a6b41f6db1cb45e701
Nithesh K <124651925+NitheshNSK@users.noreply.github.com>
2023-05-27 01:00:45 +0530

    Update README.m

--- README.m
+++ README.m
@@ -1,7 +1,5 @@
 # miniProject
 added to mini projects
 1.color picker game created by usind HTML Js and CSS added an eventListener and make changes using DOM manupulation
-2.TODO created by using HTML Js and CSS (on progress not completed fully)
-have to complete only one section to complete the project
-to strike the string wich is dynamically generated
-working in Xboard webpage
+2.TODO created by using HTML Js and CSS 
+3.GitHUB account searcher using [JavaScript,HTML,CSS,GitHub API]
